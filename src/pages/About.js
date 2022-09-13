4ba393e4e4f0667fe5e84d00374968201293b3b3
JulienPetit <julienpetit1@gmail.com>
2022-09-13 22:25:20 +0200

    Flag app et cour react 1

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel asperiores
+        sint corporis, maxime voluptatibus consectetur dolorem blanditiis ex,
+        repellendus impedit saepe. Odio dicta, molestias corrupti porro fugiat
+        nam sint sequi, odit aperiam nulla placeat ipsum minus voluptatem.
+        Maxime, aliquid, dignissimos eveniet, fugit atque possimus nobis
+        blanditiis autem saepe molestias quae magni natus sit obcaecati
+        reprehenderit necessitatibus dolore sunt ea voluptates velit. Nesciunt
+        at fugit nulla eaque atque sequi debitis temporibus obcaecati cupiditate
+        praesentium itaque esse, sint deleniti suscipit quo? Magnam repellendus,
+        totam necessitatibus perferendis harum blanditiis eligendi error cumque
+        quia, quibusdam voluptate dignissimos deserunt, nemo quam molestiae
+        exercitationem accusamus minima?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, ipsam
+        delectus necessitatibus officiis suscipit iste doloremque maiores neque
+        quasi dolorem debitis temporibus animi repudiandae omnis magni,
+        aspernatur eos placeat, veritatis eveniet nisi. Quisquam quam earum sint
+        iste! Nobis ad quam labore doloribus, earum voluptas quasi itaque id
+        voluptate consectetur! Nisi eaque corporis, error reprehenderit
+        reiciendis quidem nobis aliquid consequuntur labore fugit minima atque
+        adipisci vitae voluptate ab soluta quia ex, omnis alias rerum. Nihil
+        reiciendis aliquid autem sunt amet sint dolore quod, doloribus accusamus
+        maxime ipsa corrupti, veniam voluptas cumque, eligendi expedita. Quidem,
+        adipisci expedita sunt amet recusandae mollitia. Facere, necessitatibus
+        tempore temporibus animi nihil ad porro exercitationem laboriosam quasi
+        corporis distinctio ea cumque velit numquam deleniti corrupti sunt
+        possimus, laborum ratione modi dolore! Minus aspernatur eos quisquam
+        modi veniam molestias optio, sequi ullam natus ipsum suscipit
+        temporibus, id saepe. Aut adipisci culpa nihil doloremque a, animi
+        beatae praesentium fuga odit dicta, modi cupiditate repellat unde quas.
+        Incidunt cupiditate aliquam recusandae velit autem consequuntur
+        quisquam, inventore quidem omnis, tempore excepturi beatae accusamus
+        nostrum hic reprehenderit non sapiente ullam voluptates aspernatur vero
+        doloribus optio dolor dolorum. Dolorem repellat delectus magni quidem!
+        Consequuntur dignissimos est ea, blanditiis consequatur quos atque dicta
+        tempora?
+      </p>
+    </div>
+  );
+};
+
+export default About;
